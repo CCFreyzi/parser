8ef7212bfbd53a5ce4e6d8d29d845407a6ce8b48
anton <mamant4444@gmail.com>
2024-03-10 19:00:19 +0200

    Fix imports and improve project structure
    - Fixed imports for GPT prompt submission and parsing functionality.
    - Moved API key to a .env file for better security and configurability.
    - Added dotenv for environment variable management.

--- parsers_function.js
+++ parsers_function.js
@@ -1,29 +1,33 @@
-const getPageTitle = async (page) => {
+export const getPageTitle = async (page) => {
     return await page.evaluate(() => {
-        return 'H1: ' + document.querySelector('.page-title h1').innerHTML.replace(/^\s+|\s+$/g, '')
+        return (
+            "H1: " +
+            document
+                .querySelector(".page-title h1")
+                .innerHTML.replace(/^\s+|\s+$/g, "")
+        );
     });
 };
 
-const getMeinInf = async (page) => {
+export const getMeinInf = async (page) => {
     const meinInf = await page.evaluate(() => {
-        const parentDiv = document.querySelector('.coincodex-content').children;
+        const parentDiv = document.querySelector(".coincodex-content").children;
 
         const postContent = [];
 
         for (let i = 0; i < parentDiv.length; i++) {
             const child = parentDiv[i];
-            if (child.tagName === 'P' || child.tagName === 'H2') {
+            if (child.tagName === "P" || child.tagName === "H2") {
                 postContent.push(`${child.tagName}: ${child.textContent}`);
-            } else if (child.tagName === 'FIGURE') {
-                postContent.push(`${child.children[0].children[0].tagName}: ${child.children[0].children[0].src}`);
+            } else if (child.tagName === "FIGURE") {
+                postContent.push(
+                `${child.children[0].children[0].tagName}: ${child.children[0].children[0].src}`
+            );
             }
         }
 
-        return  postContent.join('\n');
+        return postContent.join("\n");
     });
 
     return meinInf;
 };
-
-
-module.exports = {getPageTitle, getMeinInf};
